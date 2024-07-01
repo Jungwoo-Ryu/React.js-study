2928a24341ad4897ee2c268340779d15a1e596cc
kenryu <jryu1521@gmail.com>
2024-07-01 20:07:27 +0900

    async

--- 01_homework/src/App.js
+++ 01_homework/src/App.js
@@ -1,3 +1,5 @@
+import React, {Component} from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
