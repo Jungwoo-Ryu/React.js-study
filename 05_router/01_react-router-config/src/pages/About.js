d3752fa04046253c2cea8e96ce7b1d54b215f085
kenryu <jryu1521@gmail.com>
2024-07-02 19:06:00 +0900

    params

---
+++ 05_router/01_react-router-config/src/pages/About.js
@@ -0,0 +1,11 @@
+// 소개페이지
+
+function About() {
+    return(
+        <div>
+            <h1>나는 조평훈이야</h1>
+        </div>
+    )
+}
+
+export default About;
---
+++ 05_router/01_react-router-config/src/pages/Menu.js
@@ -0,0 +1,9 @@
+function Menu(){
+    return (
+        <div>
+            <h1>나는 메뉴 페이지야</h1>
+        </div>
+    );
+}
+
+export default Menu;
